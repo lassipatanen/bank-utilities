931694337751412450e81f9453afe903c9e98d88
lassipatanen <lassipatanen@gmail.com>
2017-09-21 12:12:26 +0300

    Started working on bank-utilities project.

---
+++ bank-utility/bank-utility/BBAN.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace bank_utility
+{
+    public class BBAN
+    {
+        private static bool Validate(string userBankNumber)
+        {
+            string bankNumber = BBAN.FormatBBAN(userBankNumber);
+            if (bankNumber.Length <= 14)
+                return true;
+            else
+                return false;
+        }
+
+        public static bool VerifyCheckDigit(string userBankNumber)
+        {
+            string bankNumber = BBAN.FormatBBANToMachine(userBankNumber);
+
+            // käy pankkinumero läpi 13. luvusta alkaen alaspäin
+            // onko luvun sijainti parillinen vai ei
+            // jos pariton, kerro luku kahdella ja vähennä siitä 9 ja lisää se kokonaissummaan
+            // ja parillinen, kerro luku yhdellä ja lisää se kokonaissumaan
+        }
+
+        private static string FormatBBAN(string userBankNumber)
+        {
+            string bankNumber = userBankNumber;
+            bankNumber = bankNumber.Trim();
+            bankNumber = bankNumber.Replace(" ", "");
+            bankNumber = bankNumber.Replace("-", "");
+            return bankNumber;
+        }
+
+        public static string FormatBBANToMachine(string userBankNumber)
+        {
+            if (BBAN.Validate(userBankNumber))
+            {
+                string bankMachineNumber = BBAN.FormatBBAN(userBankNumber);
+                while (bankMachineNumber.Length < 14)
+                {
+                    if (bankMachineNumber.StartsWith("4") || bankMachineNumber.StartsWith("5"))
+                        bankMachineNumber = bankMachineNumber.Insert(7, "0");
+                    else
+                        bankMachineNumber = bankMachineNumber.Insert(6, "0");
+                }
+                return bankMachineNumber;
+            }
+            else
+                return "error";
+        }
+    }
+}
---
+++ bank-utility/bban-validator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using bank_utility;
+
+namespace bban_validator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string myBankNumber = "562009-2134961";
+            //string myBankNumber = "159030-776";
+
+            Console.WriteLine("My bank number is: {0}", myBankNumber);
+            Console.WriteLine("My machine format bank number is: {0}", BBAN.FormatBBANToMachine(myBankNumber));
+
+            // Digit Check
+            Console.WriteLine("Verifying Check Digit...");
+            if (BBAN.VerifyCheckDigit(myBankNumber))
+            {
+                Console.WriteLine("Bank number is valid");
+            }
+            else
+            {
+                Console.WriteLine("Bank number is not valid");
+            }
+
+
+
+            // END
+            Console.WriteLine("\nPress any key to continue....");
+            Console.ReadKey();
+        }
+    }
+}
